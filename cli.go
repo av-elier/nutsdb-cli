b094f182c5a37b5d21dced1ad1cc26d56bdc942e
Veremeenko Artyom <arwer13@gmail.com>
2020-05-26 19:05:53 +0300

    add cli "debug" command and draft of ListKeys function

--- cli.go
+++ cli.go
@@ -18,8 +18,21 @@ func main() {
 	var args args
 	app := &cli.App{
 		Name: "nutsdb-cli",
+		Commands: []cli.Command{
+			{
+				Name:  "debug",
+				Usage: "Create debug database 'test.db' in cwd",
+				Action: func(c *cli.Context) error {
+					cwd, _ := os.Getwd()
+					impl := db.Db{DbDir: cwd + "/test.db"}
+					impl.CreateDebugDb()
+					return nil
+				},
+			},
+		},
 	}
 	app.Flags = []cli.Flag{
+		// TODO: make this argument required
 		cli.StringFlag{
 			Name:        "db",
 			Usage:       "path to nutsdb batabase",
@@ -27,9 +40,6 @@ func main() {
 		},
 	}
 	app.Action = func(c *cli.Context) error {
-		impl := db.Db{DbDir: "c:/Users/user/projects/nutsdb-cli/data"}
-		impl.CreateDebugDb()
-		fmt.Println(impl.ListBuckets())
 		comm := comm.Communicator{}
 
 		// fmt.Println("hello nutsdb", db, comm)
--- db/db_interface.go
+++ db/db_interface.go
@@ -8,7 +8,7 @@ import (
 
 type DB interface {
 	ListBuckets() []string
-	// ListKeys(bucket string) []string
+	ListKeys(bucket string) []string
 	// Get(bucket, key string) []string
 }
 
@@ -16,6 +16,7 @@ type Db struct {
 	DbDir string
 }
 
+// CreateDebugDb just creates some non-empty database for debug purposes
 func (file Db) CreateDebugDb() {
 	opt := nutsdb.DefaultOptions
 	opt.Dir = file.DbDir
@@ -24,13 +25,22 @@ func (file Db) CreateDebugDb() {
 		log.Fatal("Cannot open database")
 	}
 
+	bucket := "bucket1"
+	keysAndValues := []struct {
+		Key   []byte
+		Value []byte
+	}{
+		{[]byte("k1"), []byte("umbrella")},
+		{[]byte("k2"), []byte("budger")},
+	}
+
 	if err := db.Update(
 		func(tx *nutsdb.Tx) error {
-			key := []byte("name1")
-			val := []byte("val1")
-			bucket := "bucket1"
-			if err := tx.Put(bucket, key, val, 0); err != nil {
-				return err
+			for _, kv := range keysAndValues {
+				if err := tx.Put(bucket, kv.Key, kv.Value, 0); err != nil {
+					log.Fatalln(err)
+					return err
+				}
 			}
 			return nil
 		}); err != nil {
@@ -40,10 +50,12 @@ func (file Db) CreateDebugDb() {
 	defer db.Close()
 }
 
+// ListBuckets returns all buckets in given database
 func (file Db) ListBuckets() []string {
 	opt := nutsdb.DefaultOptions
 	opt.Dir = file.DbDir
 	db, err := nutsdb.Open(opt)
+	defer db.Close()
 	if err != nil {
 		return []string{}
 	}
@@ -51,6 +63,41 @@ func (file Db) ListBuckets() []string {
 	for k := range db.BPTreeIdx {
 		buckets = append(buckets, k)
 	}
+	return buckets
+}
+
+// ListKeys returns all keys in given bucket
+func (file Db) ListKeys(bucket string) []string {
+	opt := nutsdb.DefaultOptions
+	opt.Dir = file.DbDir
+	db, err := nutsdb.Open(opt)
 	defer db.Close()
+
+	if err != nil {
+		return nil
+	}
+
+	buckets := []string{}
+
+	if err := db.View(
+		func(tx *nutsdb.Tx) error {
+			prefix := []byte{}
+			// Constrain 100 entries returned
+			// bucketBin := []byte(bucket)
+			limit := 1000 // TODO
+			if entries, err := tx.PrefixScan(bucket, prefix, limit); err != nil {
+				log.Println("Error occured while scanning for keys", err)
+				return nil
+			} else {
+				for _, entry := range entries {
+					keyAsString := string(entry.Key)
+					buckets := append(buckets, keyAsString)
+				}
+			}
+			return nil
+		}); err != nil {
+		log.Println(err)
+	}
+
 	return buckets
 }
